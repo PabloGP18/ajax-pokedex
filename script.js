fcab877c4a084b980076300984709a9d76f1f4f0
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-05-23 12:42:56 +0200

    dislpaying in log = name, id, 4 moves and img_link pokemon

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
     // your code here
-
-    const poke_url = "https://pokeapi.co/api/v2/pokemon/";
-
-
     /*(async ()=>{
         let data = await getData(url));
         console.log(data)
     })();*/
 
-    async function pokemonInfo () {
 
-        let data =  await fetch(poke_url)
-        let main =   await data.json()
-        console.log(main)
-    };
+    document.getElementById('run').addEventListener('click',pokemonInfo);
 
-    pokemonInfo();
+    async function pokemonInfo (){
+
+        const poke_url = "https://pokeapi.co/api/v2/pokemon/";
+        let inputPokeId = document.getElementById("poke-id").value;
+        let inputNr = Number(inputPokeId);
+
+
+        let data = await fetch(`${poke_url}${inputNr}`)
+        let response = await data.json();
+        console.log(response)
+        console.log(response.name, response.id, response.moves[0],response.moves[1],response.moves[2],response.moves[3],response.moves[4],response.sprites.front_default);
+
+    }
