4f01bef82024768617d3f230ec9479d7c40556d3
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-05-23 15:46:30 +0200

    adding image function

--- script.js
+++ script.js
@@ -15,7 +15,22 @@
 
         let data = await fetch(`${poke_url}${input}`)
         let response = await data.json();
+
+        let name = document.querySelector(".name");
+        name.textContent = response.name;
+
+        let id = document.querySelector(".id");
+        id.textContent = response.id;
+
+        let moves = document.querySelector(".moves");
+        moves.textContent = `${response.moves[0].move.name}, ${response.moves[1].move.name}, ${response.moves[2].move.name}, ${response.moves[3].move.name}`;
+
+        let pokeImage = new Image(300,300);
+        pokeImage.src = response.sprites.front_shiny;
+        document.querySelector(".sprites").appendChild(pokeImage);
+
         console.log(response)
         console.log(response.name, response.id, response.moves[0],response.moves[1],response.moves[2],response.moves[3],response.moves[4],response.sprites.front_shiny);
     }
+
 
