466c30f7f01b0c7cacf6fa9c741a35215150620c
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-05-23 14:26:50 +0200

    you can now input name to get pokemon / just removed Number from variable

--- script.js
+++ script.js
@@ -10,13 +10,12 @@
     async function pokemonInfo (){
 
         const poke_url = "https://pokeapi.co/api/v2/pokemon/";
-        let inputPokeId = document.getElementById("poke-id").value;
-        let inputNr = Number(inputPokeId);
+        let inputPokeInfo = document.getElementById("poke-id").value;
+        let input = inputPokeInfo;
 
-
-        let data = await fetch(`${poke_url}${inputNr}`)
+        let data = await fetch(`${poke_url}${input}`)
         let response = await data.json();
         console.log(response)
-        console.log(response.name, response.id, response.moves[0],response.moves[1],response.moves[2],response.moves[3],response.moves[4],response.sprites.front_default);
-
+        console.log(response.name, response.id, response.moves[0],response.moves[1],response.moves[2],response.moves[3],response.moves[4],response.sprites.front_shiny);
     }
+
